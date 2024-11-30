ef8e5b90e4e6bdd7c736a6d8014277fa4f6fb6c6
abin0526 <abinshajugit@gmail.com>
2024-11-30 13:27:43 +0530

    Add files via upload

---
+++ src/react src/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Dashboard from './component/Dashboard';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>End Day Test</h1>
+     <Dashboard/>
+    </div>
+  );
+}
+
+export default App;
